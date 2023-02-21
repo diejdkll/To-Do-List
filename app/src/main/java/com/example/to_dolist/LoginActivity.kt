99e13430dd16edcaa19bade096e255109ca8533e
diejbkll <s22026@gsm.hs.kr>
2023-02-21 16:17:28 +0900

    to-do list login

---
+++ app/src/main/java/com/example/to_dolist/LoginActivity.kt
@@ -0,0 +1,79 @@
+package com.example.to_dolist
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import com.example.to_dolist.databinding.ActivityLoginBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
+    var username: String = ""
+    var password: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val retrofitService = retrofit.create(RetrofitService::class.java)
+
+        binding.idInput.doAfterTextChanged {
+            username = it.toString()
+        }
+
+        binding.pwInput.doAfterTextChanged {
+            password = it.toString()
+        }
+
+        binding.join.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginBtn.setOnClickListener {
+            val user = HashMap<String, Any>()
+            user.put("username", username)
+            user.put("password", password)
+            retrofitService.login(user).enqueue(object : Callback<User> {
+                override fun onResponse(call: Call<User>, response: Response<User>) {
+                    if (response.isSuccessful) {
+                        val user: User = response.body()!!
+                        val sharedPreferences =
+                            getSharedPreferences("user_info", Context.MODE_PRIVATE)
+                        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                        editor.putString("token", user.token)
+                        editor.putString("user_id", user.id.toString())
+                        editor.commit()
+
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this@LoginActivity, "로그인에 실패했습니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+                override fun onFailure(call: Call<User>, t: Throwable) {
+                    Toast.makeText(this@LoginActivity, "로그인에 실패했습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+        }
+    }
+}
